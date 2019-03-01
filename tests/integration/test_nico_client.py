7de868367472743236cd471254f76bc16e392187
Jaeseo Park <jaeseopark@icloud.com>
2019-02-28 21:13:22 -0700

    added assertion for test_get_daily_trending_videos

--- tests/integration/test_nico_client.py
+++ tests/integration/test_nico_client.py
@@ -7,3 +7,7 @@ class TestNicoClient(unittest.TestCase):
     def test_get_daily_trending_videos(self):
         client = NicoClient()
         videos = client.get_daily_trending_videos()
+        for video in videos:
+            self.assertIsNotNone(video.video_id, 'video_id should not be None')
+            self.assertTrue(video.views > 0, 'video.views must be greater than 0')
+            self.assertTrue(video.likes > 0, 'video.likes must be greater than 0')
