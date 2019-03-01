a595bf3d06b80c822a380f4d98817ddf93fedc5a
Jaeseo Park <jaeseo_park@intuit.com>
2019-02-28 18:19:34 -0700

    added 'install_requires'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nico_client",
-    version="1.0.1",
+    version="1.0.4",
     author="Jaeseo Park",
     author_email="jaeseopark@icloud.com",
     description="A python client to interact with nicovideo.jp",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lekordable/nico_client",
     packages=setuptools.find_packages(),
+    install_requires=['requests', 'nicopy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
