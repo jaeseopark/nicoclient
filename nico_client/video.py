8946d85025e384263e196d730c4a798579d8b73f
Jaeseo Park <jaeseo_park@intuit.com>
2019-03-06 14:01:46 -0700

    full implementation of get_related_videos

--- nico_client/video.py
+++ nico_client/video.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 VIDEO_TYPE_UTATTEMITA = 'utattemita'
 VIDEO_TYPE_VOCALOID_ORG = 'org'
@@ -31,3 +32,20 @@ class Video(object):
             return VIDEO_TYPE_VOCALOID_ORG
         else:
             return VIDEO_TYPE_UNKNOWN
+
+    def find_references(self):
+        if self.description is None:
+            raise AssertionError('description is required')
+
+        refs = []
+        for keyword in ['sm', 'mylist/']:
+            index_set = [m.start() for m in re.finditer(keyword, self.description)]
+            for i_start in index_set:
+                i_end = None
+                for j in range(i_start + len(keyword), len(self.description)):
+                    if not self.description[j].isdigit():
+                        i_end = j
+                        break
+                refs.append(self.description[i_start:i_end])
+
+        return refs
