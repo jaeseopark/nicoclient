cc3dbf778b0cd08fca6b61fd27aadd54dc2c476f
Jaeseo Park <jaeseo_park@intuit.com>
2019-03-05 20:11:58 -0700

    support for *args, **kwargs in Video class

--- nico_client/video.py
+++ nico_client/video.py
@@ -5,21 +5,16 @@ VIDEO_TYPE_VOCALOID_ORG = 'org'
 VIDEO_TYPE_UNKNOWN = 'unknown'
 
 
-class Video:
-    def __init__(self, id=None, url=None, views=None, likes=None):
-        self.id = None
+class Video(object):
+    def __init__(self, id=None, url=None, views=None, likes=None, *args, **kwargs):
+        self.id = id
         self.views = views
         self.likes = likes
         self.thumbnail_url = None
         self.title = None
 
-        if (1 if url else 0) + (1 if id else 0) != 1:
-            raise AssertionError("'id' or 'url' needed.")
-
-        if url:
+        if url and self.id is None:
             self.id = url.split('/')[-1].split('?')[0]
-        if id:
-            self.id = id
 
     def __str__(self):
         return json.dumps(self.__dict__)
