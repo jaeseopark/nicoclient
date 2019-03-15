4e429fd8c7d17084863b9aa1cce06e03722e07f7
Jaeseo Park <jaeseopark@icloud.com>
2019-03-15 15:05:28 -0600

    handle ResponseFailError

--- nico_client/nicopy_adapter.py
+++ nico_client/nicopy_adapter.py
@@ -1,15 +1,30 @@
-from nicopy import nicopy
+from nicopy import nicopy, ResponseFailError
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_video_info(id):
-    video_info = nicopy.get_video_info(video_id=id)
-    mapped_video_info = {
-        'tags': [tag['tag'] for tag in video_info.get('tags')],
-        'description': video_info.get('description'),
-        'uploader_id': video_info.get('user_id'),
-        'title': video_info.get('title'),
-        'thumbnail_url': video_info.get('thumbnail_url'),
-        'views': int(video_info.get('view_counter')),
-        'likes': int(video_info.get('mylist_counter'))
-    }
+    try:
+        video_info = nicopy.get_video_info(video_id=id)
+        mapped_video_info = {
+            'tags': [tag['tag'] for tag in video_info.get('tags')],
+            'description': video_info.get('description'),
+            'uploader_id': video_info.get('user_id'),
+            'title': video_info.get('title'),
+            'thumbnail_url': video_info.get('thumbnail_url'),
+            'views': int(video_info.get('view_counter')),
+            'likes': int(video_info.get('mylist_counter'))
+        }
+    except ResponseFailError:
+        logger.warning(f"ResponseFailError video='{id}'; returning empty values")
+        mapped_video_info = {
+            'tags': [],
+            'description': '',
+            'uploader_id': '',
+            'title': '',
+            'thumbnail_url': '',
+            'views': 0,
+            'likes': 0
+        }
     return mapped_video_info
