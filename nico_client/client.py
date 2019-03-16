fd2a84eabaed71b4d9d3765ab27754e29d033709
Jaeseo Park <jaeseo_park@intuit.com>
2019-03-15 22:40:34 -0600

    NicoClient is not a class anymore

--- nico_client/client.py
+++ nico_client/client.py
@@ -8,27 +8,29 @@ from nico_client.video_finder import VideoFinder
 logger = logging.getLogger(__name__)
 
 
-class NicoClient(object):
-    def get_daily_trending_videos(self):
-        return DailyTrending().get_videos()
+def get_daily_trending_videos():
+    return DailyTrending().get_videos()
 
-    def populate_details(self, video):
-        video.setattrs(**get_video_info(video.id))
 
-    def get_related_videos(self, video, sort_by=None, limit=None):
-        if not video.details_populated:
-            self.populate_details(video)
+def populate_details(video):
+    video.setattrs(**get_video_info(video.id))
 
-        related_videos = VideoFinder.get_related_videos(video)
 
-        if sort_by:
-            related_videos.sort(key=lambda x: vars(x)[sort_by], reverse=True)
+def get_related_videos(video, sort_by=None, limit=None):
+    if not video.details_populated:
+        populate_details(video)
 
-        if limit and limit < len(related_videos):
-            related_videos = related_videos[:limit - 1]
+    related_videos = VideoFinder.get_related_videos(video)
 
-        return related_videos
+    if sort_by:
+        related_videos.sort(key=lambda x: vars(x)[sort_by], reverse=True)
 
-    def get_videos_by_playlist_id(self, playlist_id):
-        p = Playlist(id=playlist_id)
-        return p.get_videos()
+    if limit and limit < len(related_videos):
+        related_videos = related_videos[:limit - 1]
+
+    return related_videos
+
+
+def get_videos_by_playlist_id(playlist_id):
+    p = Playlist(id=playlist_id)
+    return p.get_videos()
