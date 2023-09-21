0b98de8a5b60bd30c088fc3dfa98ff65882e797f
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2023-09-21 00:04:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jaeseopark/nicoclient",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
-    install_requires=['requests', 'beautifulsoup4', 'python-dateutil'],
+    install_requires=['requests', 'beautifulsoup4', 'python-dateutil', 'pydantic'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Natural Language :: English"
